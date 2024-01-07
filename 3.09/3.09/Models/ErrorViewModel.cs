d71e3c0b60fc6134538c36bd54516b8351c1f2ef
OlexandrVorobchak <eclipsesaers@gmail.com>
2024-01-07 22:18:51 +0200

    1
    1

---
+++ 3.09/3.09/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _3._09.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
